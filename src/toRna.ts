fbd63cb9fe08032c7ea60cd77d5981dffd255a1a
blacktoast <dbstjd100@naver.com>
2021-09-06 15:58:17 +0900

    1문제 해결 toRna 문제 해결

--- src/toRna.ts
+++ src/toRna.ts
@@ -6,20 +6,18 @@ let convertDnatoRna = (d) => {
 };
 
 export function toRna(dn) {
-  let chk;
+  let chk = false;
   let rna = [...dn];
-  rna.map(convertDnatoRna).map((a) => {
-    a === undefined ? (chk = false) : a;
+  let t = rna.map(convertDnatoRna).map((a) => {
+    a === undefined ? (chk = true) : a;
+    return a;
   });
-  //console.log(rna);
 
   if (chk) {
     throw new Error("Invalid input DNA.");
   } else {
-    return rna.join("");
+    return t.join("");
   }
 }
 let a = "AAC";
-let b = [...a];
-b.map(convertDnatoRna);
-console;
+console.log(a.split(""));
